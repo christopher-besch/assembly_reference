d9983980baa6e93980448f4b12895d1f18784a51
Christopher Besch <christopher.besch@gmx.de>
2021-06-03 13:44:30 +0200

    null terminated string working

---
+++ 05_null_termination/main.asm
@@ -0,0 +1,45 @@
+section .data
+    text db "Hello World!",10,0
+    text1 db "I like trains.",10,0
+
+section .text
+    global _start
+
+_start:
+    mov rax, text
+    call _print
+
+    mov rax, text1
+    call _print
+
+    mov rax, 60
+    mov rdi, 0
+    syscall
+
+; print string at rax
+; rax const char*
+_print:
+    push rax
+    ; counter
+    mov rbx, 0
+
+_print_loop:
+    ; go to next character
+    inc rax
+    ; found new counter
+    inc rbx
+    ; move current char in string to least significant bit of rcx
+    mov cl, [rax]
+    cmp cl, 0
+    jne _print_loop
+
+    ; print
+    mov rax, 1
+    mov rdi, 1
+    ; address to first char
+    pop rsi
+    mov rdx, rbx
+    syscall
+
+    ret
+
